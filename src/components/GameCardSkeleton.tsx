897a2459d360010a854f050c5b0c4192ae82201a
Q-Mick <quentinmick@protonmail.com>
2023-08-09 19:12:05 -0600

    skeleton loader

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,22 @@
+import {
+	Card,
+	CardBody,
+	Image,
+	Heading,
+	Text,
+	HStack,
+	Skeleton,
+	SkeletonText,
+} from '@chakra-ui/react';
+const GameCardSkeleton = () => {
+	return (
+		<Card width="300px" borderRadius={10} overflow="hidden">
+			<Skeleton height="200" />
+			<CardBody>
+				<SkeletonText />
+			</CardBody>
+		</Card>
+	);
+};
+
+export default GameCardSkeleton;
