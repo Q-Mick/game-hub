44cd1bd4d2268c8e74e5dc8b369279a061fccb93
Q-Mick <quentinmick@protonmail.com>
2023-08-11 13:05:08 -0600

    sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
+	if (!url) return '';
 	const target = 'media/';
 	const index = url.indexOf(target) + target.length; // Get the position in the url directly after the /
 	return url.slice(0, index) + 'crop/600/400/' + url.slice(index); // Add the url to get a cropped version of the image
