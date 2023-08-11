823777705e68978f0bd5c2df0db333bc2d9d93f6
Q-Mick <quentinmick@protonmail.com>
2023-08-10 20:16:03 -0600

    update genre icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,16 +11,16 @@ const GameGrid = () => {
 	return (
 		<>
 			{error && <Text>{error}</Text>}
-			<SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding="10px">
+			<SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" spacing={3}>
 				{isLoading &&
 					skeletons.map((skeleton) => (
 						<GameCardContainer>
 							<GameCardSkeleton key={skeleton} />
 						</GameCardContainer>
 					))}
-				{data.map((game) => (
+				{data.map((data) => (
 					<GameCardContainer>
-						<GameCard key={game.id} game={game} />
+						<GameCard key={data.id} game={data} />
 					</GameCardContainer>
 				))}
 			</SimpleGrid>
