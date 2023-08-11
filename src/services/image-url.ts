e0e6e5aaec3af054fe08afcf571f45dedd4c0421
Q-Mick <quentinmick@protonmail.com>
2023-08-11 13:20:22 -0600

    handle No Image for game

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url: string) => {
-	if (!url) return '';
+	if (!url) return noImage;
 	const target = 'media/';
 	const index = url.indexOf(target) + target.length; // Get the position in the url directly after the /
 	return url.slice(0, index) + 'crop/600/400/' + url.slice(index); // Add the url to get a cropped version of the image
