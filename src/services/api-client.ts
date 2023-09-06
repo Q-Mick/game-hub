3f0fc737acabfa472f82988a74b805f03e227447
Q-Mick <quentinmick@protonmail.com>
2023-09-06 11:43:00 -0600

    apiclient added

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
 	count: number;
 	results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
 	baseURL: 'https://api.rawg.io/api',
 	params: {
 		key: 'c10295c7f83d492e8e80760440925e07',
 	},
 });
+
+class APIClient<T> {
+	endpoint: string;
+
+	constructor(endpoint: string) {
+		this.endpoint = endpoint;
+	}
+
+	getAll = (config: AxiosRequestConfig) => {
+		return axiosInstance
+			.get<FetchResponse<T>>(this.endpoint, config)
+			.then((res) => res.data);
+	};
+}
+
+export default APIClient;
