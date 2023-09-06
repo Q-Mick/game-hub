2917a6bd408fa433a98a92308150d0a429a6b043
Q-Mick <quentinmick@protonmail.com>
2023-09-06 11:25:06 -0600

    got rid of useData, leveraging react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
+
+export interface FetchResponse<T> {
+	count: number;
+	results: T[];
+}
 
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
-  params: {
-    key: 'c10295c7f83d492e8e80760440925e07'
-  }
-})
+	baseURL: 'https://api.rawg.io/api',
+	params: {
+		key: 'c10295c7f83d492e8e80760440925e07',
+	},
+});
