33b4df2dd36804ae463603b2b1845400be56052a
Q-Mick <quentinmick@protonmail.com>
2023-09-15 18:02:01 -0600

    added platform and genre hook

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -7,6 +7,7 @@ import {
 } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 import usePlatforms, { Platform } from '../hooks/usePlatforms';
+import usePlatform from '../hooks/usePlatform';
 
 interface Props {
 	onSelectPlatform: (platform: Platform) => void;
@@ -17,9 +18,7 @@ const PlatformSelector = ({
 	selectedPlatformId,
 }: Props) => {
 	const { data, error } = usePlatforms();
-	const selectedPlatform = data?.results.find(
-		(p) => p.id === selectedPlatformId
-	);
+	const selectedPlatform = usePlatform(selectedPlatformId);
 
 	if (error) {
 		return null;
